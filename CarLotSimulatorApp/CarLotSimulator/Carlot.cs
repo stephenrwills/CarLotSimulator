5b20a5829816eb6d37af73da09d349f59f2a341a
Stephen Wills <stephenrwills94@gmail.com>
2022-07-13 16:43:31 -0500

    finished main method

---
+++ CarLotSimulatorApp/CarLotSimulator/Carlot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Carlot
+    {
+        public List<Car> Cars = new List<Car>();
+    }
+}
